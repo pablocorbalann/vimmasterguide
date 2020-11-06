27316886133cd62dfb6f9e1a61327242d2d40edc
pablo <corbalandeconpablo@gmail.com>
2020-11-06 08:37:30 +0100

    feat: Specific chapter selection created

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def chapters():
 def license():
     return render_template('license.html', title="License", bar="vmg/license")
 
+@app.route('/c/<id>')
+def chapter(id: int):
+    return render_template(f'chapters/{id}.html', bar="vmg/chapters/1")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
