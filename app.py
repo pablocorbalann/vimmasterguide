d7b918874371fcb086720668eb1b7f21383aeffb
pablo <corbalandeconpablo@gmail.com>
2020-11-06 16:59:19 +0100

    feat: Basic HTTP errors implemented

--- app.py
+++ app.py
@@ -31,12 +31,44 @@ def chapter(cid:int):
                 # set the title of the page as the title of the chapter
                 t = c[0]
                 return render_template(f'chapters/{cid}.html', title=t, bar=f"vgm/chapters/{cid}")
-    return redirect(url_for('e404'))
+    return url_for('error404')
 
-@app.route('/e/404')
+
+@app.route('/error/400')
+@app.errorhandler(400)
+def error400(err=None):
+	e = {
+	    'number':'400',
+	    'description':'The request that this page has received is not valid. Perhaps your browser has been confused or you are using the wrong method to access this page.'
+	}
+	return render_template('error.html', title='400', bar="vgm/400", error=e), 400
+
+@app.route('/error/401')
+@app.errorhandler(401)
+def error401(err=None):
+	e = {
+	    'number':'401',
+	    'description':'We are sorry but you do not have authorization to access this page.'
+	}
+	return render_template('error.html', title='401', bar="vmg/401", error=e), 401
+
+@app.route('/error/404')
 @app.errorhandler(404)
-def e404(err=None):
-    return '<h1>404</h1>', 404
+def error404(err=None):
+	e = {
+	    'number':'404',
+	    'description':'We could not find the page you are looking for, perhaps you have misspelled the link or the page is private.'
+	}
+	return render_template('error.html', title='404', bar="vmg/404", error=e), 404
+
+@app.route('/error/500')
+@app.errorhandler(500)
+def error500(err=None):
+	e = {
+	    'number':'500',
+	    'description':'Do not worry, this error is not your fault. There has been an internal error on our server. We are trying to fix it.'
+	}
+	return render_template('error.html', title='500', bar="vmg/500", error=e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
